b616c0121d6324e6c56db37907eebd0217992405
jadtay <49256300+jadtay@users.noreply.github.com>
2020-04-26 10:27:30 -0400

    mostly finished just need to cleanup

--- teacher/test.py
+++ teacher/test.py
@@ -1,18 +1,61 @@
 from flask import Flask, render_template, request, session
+import pyrebase
 import json
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] ='b1gk3y'
 
+config = {
+  "apiKey": "AIzaSyDSdsT2h6wAlkQI3rj7sAbPD2a0hFJVIIA",
+  "authDomain": "mathtutorapp-f3ddc.firebaseapp.com",
+  "databaseURL": "https://mathtutorapp-f3ddc.firebaseio.com",
+  "storageBucket": "mathtutorapp-f3ddc.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+auth = firebase.auth()
+db = firebase.database()
+
+def signIn(email,password):
+    user = auth.sign_in_with_email_and_password(email,password)
+    return user
+
+def signUp(email,password):
+    auth.create_user_with_email_and_password(email,password)
+
 class lessons:
-    def __init__(self, topic, video, quiz):
+    def __init__(self, topic, video, quiz, quizno):
         self.topic = topic
         self.video = video
         self.quiz = quiz
+        self.quizno = quizno
 
-@app.route("/")
+@app.route("/", methods = ['POST','GET'])
+def index():
+    return render_template('index.html')
+
+@app.route("/signin", methods = ['POST'])
+def signin():
+    email = request.form.get("email_in")
+    password = request.form.get("password_in")
+    user = signIn(email,password)
+    session['user'] = user
+    username = email.split('@')[0]
+    session['username'] = username
+    return render_template('home.html', email = email)
+
+@app.route("/signup", methods = ['POST'])
+def signup():
+    email = request.form.get("email_up")
+    password = request.form.get("password_up")
+    signUp(email,password)
+    return render_template('signup.html')
+
+@app.route("/lessoncreator", methods = ['POST'])
 def test():
     return render_template('test.html')
+
 
 @app.route('/quizcreator', methods = ['POST'])
 def quizcreator():
@@ -29,17 +72,45 @@ def complete():
     lesson = session.get('lesson')
     video = session.get('video')
     noquestions = session.get('noquestions')
+    user = session.get('user')
+    username = session.get('username')
     qDict = {}
     for i in range(noquestions):
         question = request.form.get("q{}".format(i+1))
-        print(question)
         answer = request.form.get("a{}".format(i+1))
-        print(answer)
         qDict[question] = answer
-    l1 = lessons(lesson,video,qDict)
+    l1 = lessons(lesson,video,qDict,noquestions)
     y = json.dumps(l1.__dict__)
-    
-    return render_template('test3.html', lessonplan = y, lessonplan1 = l1)
+    db.child("users").child(username).child(lesson).set(y, user['idToken'])
+    return render_template('complete.html')
+
+@app.route("/collection", methods = ['POST'])
+def collection():
+    username = session.get('username')
+    lessonset = db.child("users").child(username).get()
+    lessonCollection = []
+    for lesson in lessonset.each():
+        lessonCollection.append(lesson.key())
+    return render_template('collection.html',username = username, lessonCollection = lessonCollection)
+
+@app.route("/lesson", methods = ['POST'])
+def lessoner():
+    username = session.get('username')
+    select = request.form.get("selector")
+    print(username)
+    print(select)
+    x = db.child("users").child(username).child(select).get().val()
+    print(x)
+    display = json.loads(x)
+    final = lessons(display["topic"], display["video"], display["quiz"], display["quizno"])
+    return render_template('test3.html', lessonplan1 = final)
+
+z = db.child("users").child("yaboi").child("algebra").get().val()
+display = json.loads(z)
+final = lessons(display["topic"], display["video"], display["quiz"], display["quizno"])
+print(final.topic)
+for question in final.quiz.keys():
+    print(question)
 
 if __name__ == '__main__':
     app.run(debug=True)
