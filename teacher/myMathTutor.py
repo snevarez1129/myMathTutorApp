cb8b783d8054e774cb6f4a9853a19e4b862da834
jadtay <49256300+jadtay@users.noreply.github.com>
2020-05-01 13:16:20 -0400

    final cleanup

--- teacher/myMathTutor.py
+++ teacher/myMathTutor.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] ='b1gk3y'
 
+#configure initial firebase settings
 config = {
   "apiKey": "",
   "authDomain": "mathtutorapp-f3ddc.firebaseapp.com",
@@ -13,18 +14,22 @@ config = {
   "storageBucket": "mathtutorapp-f3ddc.appspot.com"
 }
 
+#use pYrebase package to use firebase through python and flask
 firebase = pyrebase.initialize_app(config)
 
 auth = firebase.auth()
 db = firebase.database()
 
+#signin function using pyrebase
 def signIn(email,password):
 	user = auth.sign_in_with_email_and_password(email,password)
 	return user
 
+#signup function using pyrebase
 def signUp(email,password):
     auth.create_user_with_email_and_password(email,password)
 
+#class used to store lessons
 class lessons:
     def __init__(self, topic, video, quiz, quizno):
         self.topic = topic
@@ -32,10 +37,12 @@ class lessons:
         self.quiz = quiz
         self.quizno = quizno
 
+#landing page for login/signup
 @app.route("/", methods = ['POST','GET'])
 def index():
     return render_template('index.html')
 
+#signup page
 @app.route("/new_user", methods = ['POST'])
 def signup():
     email = request.form.get("email_up")
@@ -43,6 +50,7 @@ def signup():
     signUp(email,password)
     return render_template('signup.html')
 
+#home page after login
 @app.route("/home", methods = ['POST','GET'])
 def signin():
 	if request.method == "POST":
@@ -57,10 +65,12 @@ def signin():
 		usr = session.get('username')
 		return render_template('home.html', username=usr)
 
+#lesson creator page
 @app.route("/lessoncreator", methods = ['POST'])
 def test():
-    return render_template('test.html')
+    return render_template('lessoncreator.html')
 
+#quiz creator page
 @app.route('/quizcreator', methods = ['POST'])
 def quizcreator():
     lesson = request.form.get("lesson")
@@ -69,8 +79,9 @@ def quizcreator():
     session['lesson'] = lesson
     session['video'] = video
     session['noquestions'] = noquestions
-    return render_template('test2.html', noquestions = noquestions)
+    return render_template('quizcreator.html', noquestions = noquestions)
 
+#quiz complete page
 @app.route("/complete", methods = ['POST'])
 def complete():
     lesson = session.get('lesson')
@@ -79,15 +90,21 @@ def complete():
     user = session.get('user')
     username = session.get('username')
     qDict = {}
+
     for i in range(noquestions):
         question = request.form.get("q{}".format(i+1))
         answer = request.form.get("a{}".format(i+1))
         qDict[question] = answer
+    
+    #make lesson class
     l1 = lessons(lesson,video,qDict,noquestions)
     y = json.dumps(l1.__dict__)
+
+    #add lesson to database under user
     db.child("users").child(username).child(lesson).set(y, user['idToken'])
     return render_template('complete.html')
 
+#lesson collection page
 @app.route("/collection", methods = ['POST'])
 def collection():
     username = session.get('username')
@@ -97,6 +114,7 @@ def collection():
         lessonCollection.append(lesson.key())
     return render_template('collection.html',username = username, lessonCollection = lessonCollection)
 
+#view lesson page
 @app.route("/lesson", methods = ['POST'])
 def lessoner():
     username = session.get('username')
@@ -107,7 +125,7 @@ def lessoner():
     print(x)
     display = json.loads(x)
     final = lessons(display["topic"], display["video"], display["quiz"], display["quizno"])
-    return render_template('test3.html', lessonplan1 = final)
+    return render_template('lesson.html', lessonplan1 = final)
 
 if __name__ == '__main__':
     app.run(debug=True)
